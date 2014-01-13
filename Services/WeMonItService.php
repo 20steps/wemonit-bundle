b1a99a937ff72f05b42014c39d7f137030f691f3
Helmut Hoffer von Ankershoffen <helmuthva@20steps.de>
2014-01-13 07:55:58 +0100

    update

--- Services/WeMonItService.php
+++ Services/WeMonItService.php
@@ -38,16 +38,26 @@ class WeMonItService {
         $service=null;
         if (false === ($service = $this->cache->fetch($cacheKey))) {
             $this->logger->info('Calling WeMonIt API: '.$call);
-	        $request = $this->client->get($call,
-	            array(), 
-	            array(
-	                'timeout'         => $this->timeout,
-	                'connect_timeout' => $this->connectTimeout
-	        ));
+            try {
+		        $request = $this->client->get($call,
+		            array(), 
+		            array(
+		                'timeout'         => $this->timeout,
+		                'connect_timeout' => $this->connectTimeout
+		        ));
+		    } catch (\Exception $e) {
+		    	$this->logger->info($e->getMessage());
+		    	return null;
+		    } catch (Guzzle\Http\Exception\CurlException $e){
+		    	$this->logger->info($e->getMessage());
+		    	return null;
+		    }
 	        $response=$request->send()->json();
 	        $service=$response;
-	        $service['images']['uptime4']='http://www.wemonit.de/'.$service['images']['uptime4'];
-	        $service['images']['latency4']='http://www.wemonit.de/'.$service['images']['latency4'];
+	        if (array_key_exists('images',$service)) {
+		        $service['images']['uptime4']='http://www.wemonit.de/'.$service['images']['uptime4'];
+		        $service['images']['latency4']='http://www.wemonit.de/'.$service['images']['latency4'];	        	
+	        }
 	        $this->logger->info("WeMonIt returned ".var_export($service,true));
             $this->cache->save($cacheKey, $service, $this->cacheTTL);
         }
@@ -59,15 +69,89 @@ class WeMonItService {
     	if ($this->stopwatch) { $this->stopwatch->start('twentysteps\Bundle\WeMonItBundle\Services\WeMonItService.getServices','20steps'); };
         $call="service/list?apikey=".$this->apiKey;
 		$this->logger->info('Calling WeMonItAPI: '.$call);
-        $request = $this->client->get($call,
-            array(), 
-            array(
-                'timeout'         => $this->timeout,
-                'connect_timeout' => $this->connectTimeout
-        ));
-        $response=$request->send()->json();
+		try {
+	        $request = $this->client->get($call,
+	            array(), 
+	            array(
+	                'timeout'         => $this->timeout,
+	                'connect_timeout' => $this->connectTimeout
+	        ));
+	        $response=$request->send()->json();
+        } catch (\Exception $e) {
+	    	$this->logger->info($e->getMessage());
+	    	return null;
+	    } catch (Guzzle\Http\Exception\CurlException $e){
+	    	$this->logger->info($e->getMessage());
+	    	return null;
+	    }
 	    $this->logger->info("WeMonIt returned ".var_export($service,true));
     	if ($this->stopwatch) { $this->stopwatch->start('twentysteps\Bundle\WeMonItBundle\Services\WeMonItService.getServices','20steps'); };
         return $response;
     }
+
+    public function getIPv4UptimePercent($id) {
+    	$service=$this->getService($id);
+    	if ($service && array_key_exists('stats',$service)) {
+    		return $service['stats']['uptimePercent4'];
+    	}
+    	return null;
+    }
+
+    public function getIPv4DowntimePercent($id) {
+    	$service=$this->getService($id);
+    	if ($service && array_key_exists('stats',$service)) {
+    		return $service['stats']['downtimePercent4'];
+    	}
+    	return null;
+    }
+
+    public function getIPv4Downtime($id) {
+    	$service=$this->getService($id);
+    	if ($service && array_key_exists('stats',$service)) {
+    		return $service['stats']['downtime4'];
+    	}
+    	return null;
+    }
+
+    public function getAge($id) {
+    	$service=$this->getService($id);
+    	//var_dump($service);
+    	if ($service && array_key_exists('stats',$service)) {
+    		return $service['stats']['age'];
+    	}
+    	return null;
+    }
+
+    public function getIPv4ResponseDuration($id) {
+    	$service=$this->getService($id);
+    	if ($service && array_key_exists('stats',$service)) {
+    		return $service['current'];
+    	}
+    	return null;
+    }
+
+    public function getIPv6ResponseDuration($id) {
+    	$service=$this->getService($id);
+    	if ($service && array_key_exists('stats',$service)) {
+    		return $service['current6'];
+    	}
+    	return null;
+    }
+
+    public function getIPv4LastDowntime($id) {
+    	$service=$this->getService($id);
+    	if ($service && array_key_exists('stats',$service)) {
+    		return $service['last_error'];
+    	}
+    	return null;
+    }
+
+    public function getIPv6LastDowntime($id) {
+    	$service=$this->getService($id);
+    	if ($service && array_key_exists('stats',$service)) {
+    		return $service['last_error6'];
+    	}
+    	return null;
+    }
+    
  }
